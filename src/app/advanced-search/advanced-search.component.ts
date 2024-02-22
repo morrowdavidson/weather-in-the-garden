0d0e71b63945597b680f58cc53b38308eea1b9e5
Justin Davidson <76005901+JustinDavidsonStruxure@users.noreply.github.com>
2024-02-22 16:48:12 -0600

    Start development of advanced search component and form

--- src/app/advanced-search/advanced-search.component.ts
+++ src/app/advanced-search/advanced-search.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../api.service';
+
+interface FormValues {
+  location: string;
+}
+
+interface WeatherStat {
+  precipitation: number;
+  mintemp?: number;
+  maxtemp?: number;
+  date: Date;
+}
 
 @Component({
   selector: 'app-advanced-search',
   templateUrl: './advanced-search.component.html',
-  styleUrls: ['./advanced-search.component.scss']
+  styleUrls: ['./advanced-search.component.scss'],
 })
-export class AdvancedSearchComponent {
+export class AdvancedSearchComponent implements OnInit {
+  public advancedSearchForm!: FormGroup;
+  public weatherData: any;
+  public forecastData: any;
+  public totalPrecipitation: number = 0;
+  public weatherStats: Record<string, WeatherStat> = {};
+  public precipitationForecast: Record<string, WeatherStat> = {};
+  public locationData: any = {};
+  public weatherOptions: any;
+  public searchClicked: boolean = false;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiService: ApiService
+  ) {}
+  ngOnInit() {
+    this.advancedSearchForm = this.formBuilder.group({
+      location: [''],
+    });
+  }
+  submitSearch(formValues: FormValues) {
+    this.searchClicked = true;
+    //Get the historical weather data
+    this.apiService
+      .getHistorical(formValues.location, '24')
+      .subscribe((data) => {
+        this.weatherData = data;
+        console.log(this.weatherData);
+
+        this.locationData.name = this.weatherData.location.name;
+        this.locationData.region = this.weatherData.location.region;
+        this.locationData.country = this.weatherData.location.country;
+
+        console.log(this.locationData);
+
+        const dateArray = Object.keys(this.weatherData.historical);
+
+        this.totalPrecipitation = 0;
+
+        for (let i = 0; i < dateArray.length; i++) {
+          let day = 'day' + (-i - 1); // Create a string for the day
+          let precipitation =
+            this.weatherData.historical[dateArray[i]].hourly[0].precip;
+          let mintemp = this.weatherData.historical[dateArray[i]].mintemp;
+          let maxtemp = this.weatherData.historical[dateArray[i]].maxtemp;
+          let date = new Date(dateArray[i]);
+
+          this.totalPrecipitation += precipitation;
+          // Create a new property in weatherStats for the day and assign it the precipitation and temperature
+          this.weatherStats[day] = {
+            precipitation: precipitation,
+            mintemp: mintemp,
+            maxtemp: maxtemp,
+            date: date,
+          };
+        }
+        // this.setOptions();
+      });
+  }
 }
--- src/app/advanced-view/advanced-view.component.ts
+++ src/app/advanced-view/advanced-view.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-advanced-view',
   templateUrl: './advanced-view.component.html',
-  styleUrls: ['./advanced-view.component.scss']
+  styleUrls: ['./advanced-view.component.scss'],
 })
-export class AdvancedViewComponent {
-
-}
+export class AdvancedViewComponent {}
